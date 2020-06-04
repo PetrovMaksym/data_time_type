28a88f0e26c8cf469cc9168f11a22431ae22a836
Mikhail3125 <mihagribkov2001@gmail.com>
2020-06-04 23:11:25 +0300

    Добавьте файлы проекта.

---
+++ DataTimeShuvi/DataTimeShuvi.cpp
@@ -0,0 +1,16 @@
+﻿#include "Stdafx.h"
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        srand(time(NULL));
+          low();
+          midle();
+           hard();
+           return 0;
+}
+
+
